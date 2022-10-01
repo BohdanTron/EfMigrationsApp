2186d0acfd26d4f96e5ececbdf5e0d6ad1030244
tronbodya <tronbodya@gmail.com>
2022-10-01 13:30:08 +0300

    db context setup

---
+++ WebApi/DataAccess/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
