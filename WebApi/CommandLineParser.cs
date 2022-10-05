279d8c01ff4bbb641ef099240d4f00e65ba5a4c8
tronbodya <tronbodya@gmail.com>
2022-10-05 20:27:54 +0300

    command line parser

---
+++ WebApi/CommandLineParser.cs
@@ -0,0 +1,45 @@
+﻿using CommandLine;
+
+namespace WebApi
+{
+    public record DatabaseOptions
+    {
+        [Option('c', "connectionString", Required = true, HelpText = "Connection string for the database")]
+        public string ConnectionString { get; init; } = string.Empty;
+    }
+
+    public static class CommandLineParser
+    {
+        private static readonly Parser Parser;
+
+        static CommandLineParser()
+        {
+            Parser = new Parser(config =>
+            {
+                config.IgnoreUnknownArguments = true;
+                config.AutoHelp = true;
+                config.HelpWriter = Console.Error;
+            });
+        }
+
+        public static bool IsRunningMigrationMode(string[] args)
+        {
+            return args.Contains("--RunMigrations");
+        }
+
+        public static string GetConnectionString(string[] args)
+        {
+            var connectionString = Parser.ParseArguments<DatabaseOptions>(args)
+                .MapResult(
+                    options => options.ConnectionString,
+                    _ => string.Empty);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException("Command line arguments are invalid");
+            }
+
+            return connectionString;
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using WebApi;
 using WebApi.DataAccess;
+
+var runningMigrationMode = CommandLineParser.IsRunningMigrationMode(args);
+
+if (runningMigrationMode)
+{
+    var connectionString = CommandLineParser.GetConnectionString(args);
+
+    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+    optionsBuilder.UseSqlite(connectionString);
+
+    await using var dbContext = new AppDbContext(optionsBuilder.Options);
+    await dbContext.Database.MigrateAsync();
+
+    return;
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
